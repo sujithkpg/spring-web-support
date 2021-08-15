d2dd6bf691c79bd256fc91a8b9ddcc4052d5776f
Sujith Gamage <sujithkpg@gmail.com>
2021-08-15 16:46:23 +0530

    SKP:initiate the project

---
+++ com.skp/src/main/java/com/example/com/skp/SupportWebApplication.java
@@ -0,0 +1,13 @@
+package com.example.com.skp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupportWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupportWebApplication.class, args);
+	}
+
+}
---
+++ com.skp/src/test/java/com/example/com/skp/SupportWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.com.skp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupportWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
